ad7c3612dbfe9cba5a810b910591c195f376e136
Mihai Dornescu <dornescu@gmail.com>
2022-07-16 20:13:45 +0300

    first commit

---
+++ src/app/components/bind/bind.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bind',
+  templateUrl: './bind.component.html',
+  styleUrls: ['./bind.component.less']
+})
+export class BindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
