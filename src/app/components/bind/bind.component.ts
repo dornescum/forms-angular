a2b40ded8d05fb0a170d8b1feeab6ff50b3af1f5
Mihai Dornescu <dornescu@gmail.com>
2022-07-17 15:15:48 +0300

    add service localhost

--- src/app/components/bind/bind.component.ts
+++ src/app/components/bind/bind.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "src/app/components/user";
+import {EnrollmentService} from "src/app/services/enrollment.service";
 
 @Component({
   selector: 'app-bind',
@@ -7,12 +8,21 @@ import {User} from "src/app/components/user";
   styleUrls: ['./bind.component.less']
 })
 export class BindComponent implements OnInit {
-userModel = new User("Mihai",  "mihai@test", 2121555, "Angular", true);
+  userModel = new User("Mihai",  "mihai@test", 2121555, "Angular", true);
   topics: string[]= ["Angular","React","Vue"];
 
-  constructor() { }
+  constructor(private enrollmentService: EnrollmentService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    console.log(this.userModel)
+    this.enrollmentService.enroll(this.userModel).subscribe((data)=>{
+      try {
+        console.log("Succes",data)
+      } catch (error){
+        console.log("Error", error)
+      }
+    })
+  }
 }
---
+++ src/app/services/enrollment.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "src/app/components/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+    url="http://localhost:3000/enroll";
+  constructor(private _http:HttpClient) { }
+  enroll(user: User){
+    return this._http.post<any>(this.url, user);
+  }
+}
