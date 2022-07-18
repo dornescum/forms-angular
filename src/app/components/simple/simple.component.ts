af7b5fe5d2ff70012bd2d2dc4f858bef69efd2ae
Mihai Dornescu <dornescu@gmail.com>
2022-07-18 18:11:47 +0300

    fix get method simple

--- src/app/components/simple/simple.component.ts
+++ src/app/components/simple/simple.component.ts
@@ -1,47 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EnrollmentService} from "src/app/services/enrollment.service";
 import {ApiService} from "src/app/services/api.service";
 import {tick} from "@angular/core/testing";
+import {stringify} from "@angular/compiler/src/util";
 
 @Component({
-  selector: 'app-simple',
-  templateUrl: './simple.component.html',
-  styleUrls: ['./simple.component.less']
+  selector: 'app-simple', templateUrl: './simple.component.html', styleUrls: ['./simple.component.less']
 })
 export class SimpleComponent implements OnInit {
   // userName:string= '';
   // email:string = '';
-  info: any={userName:"", email:""}
-  allData:string[]=[];
-  constructor(private enrollmentService:EnrollmentService, private getApi:ApiService) { }
+  info: any = {userName: "", email: ""}
+  allData: any[] = [];
 
-  ngOnInit(): void {}
-  sendInfo(){
+  constructor(private enrollmentService: EnrollmentService, private getApi: ApiService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  sendInfo() {
     // console.log("name :  ", this.userName)
     // console.log("email: ", this.email)
     console.log(this.info.userName)
     console.log(this.info.email)
-    this.enrollmentService.enroll(this.info).subscribe((data)=>{
+    console.log(this.info)
+    this.enrollmentService.enroll(this.info).subscribe((data:{}) => {
       try {
-        console.log(data)
-        this.info.userName =''
+        console.log(typeof data)
+        this.info.userName = ''
         this.info.email = ''
-      }
-      catch (err){
+      } catch (err) {
         console.log(err)
       }
     })
   }
-  getInfo(){
-    this.getApi.getTable().subscribe(info=>{
-      console.log(info.data)
-      this.allData.push(info)
-      this.allData.forEach(item=> {
-        console.log(item)
-      })
-      })
 
-
-
+  getInfo() {
+    this.getApi.getTable().subscribe(info => {
+      // console.log(JSON.stringify(info.data))
+      this.allData = info.data;
+      // this.allData.forEach(item => {
+      //   console.log(JSON.stringify(item))
+      // })
+    })
+    // console.log(this.allData)
   }
 }
