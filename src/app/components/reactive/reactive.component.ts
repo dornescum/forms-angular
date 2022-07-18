d79c9b3522c1db5593cf2d771eb4d610b434a339
Mihai Dornescu <dornescu@gmail.com>
2022-07-18 22:24:14 +0300

    formGroup & formControl

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.less']
 })
 export class ReactiveComponent implements OnInit {
-
+  registrationForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  })
   constructor() { }
 
   ngOnInit(): void {
